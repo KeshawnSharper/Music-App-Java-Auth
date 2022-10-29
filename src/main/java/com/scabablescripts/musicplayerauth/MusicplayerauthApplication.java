5010c62c0594d8a393456e053844491747fca8ce
Keshawn Sharper <ksharper@LT-C993060M2M.hsd1.pa.comcast.net>
2022-10-29 09:36:31 -0400

    initial commit

---
+++ src/main/java/com/scabablescripts/musicplayerauth/MusicplayerauthApplication.java
@@ -0,0 +1,13 @@
+package com.scabablescripts.musicplayerauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicplayerauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicplayerauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scabablescripts/musicplayerauth/MusicplayerauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scabablescripts.musicplayerauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicplayerauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
